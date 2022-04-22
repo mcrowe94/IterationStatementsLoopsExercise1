4a70a86f5b163c4235b3548c2f05edd630bc5271
Monica Crowe <mcrowe9194@gmail.com>
2022-04-21 22:50:46 -0500

    Add project files.

---
+++ IterationStatementsLoopsExercise1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace IterationStatementsLoopsExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"1{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"2{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"3{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"4{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"5{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"6{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"7{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"8{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"9{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"10{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"11{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"12{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"13{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"14{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"15{i}");
+            }
+        }
+    }
+}
